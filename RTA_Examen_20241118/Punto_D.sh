062714b967bf4375dd26fc515c15a4abd219f313
ianvillan11 <ianvillan4@gmail.com>
2024-11-18 22:37:40 +0000

    agregado el historil

--- RTA_Examen_20241118/Punto_D.sh
+++ RTA_Examen_20241118/Punto_D.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
 
-# Crear estructura de directorios
-echo "Creando estructura de directorios..."
-mkdir -p /tmp/2do_parcial/alumno
-mkdir -p /tmp/2do_parcial/equipo
+# Definir la ubicación de los archivos en la estructura de Ansible
+DIRECTORIO_ALUMNO="/home/vagrant/repogit/UTNFRA_SO_2do_Parcial_Villan/202406/ansible/tmp/2do_parcial/alumno"
+DIRECTORIO_EQUIPO="/home/vagrant/repogit/UTNFRA_SO_2do_Parcial_Villan/202406/ansible/tmp/2do_parcial/equipo"
 
-# Generar datos del alumno
+# Asegurarse de que los directorios existen
+mkdir -p "$DIRECTORIO_ALUMNO"
+mkdir -p "$DIRECTORIO_EQUIPO"
+
 echo "Generando datos del alumno..."
-cat <<EOF > /tmp/2do_parcial/alumno/datos_alumno.txt
-Nombre: Depaolo, Juan Manuel
+cat <<EOF > "$DIRECTORIO_ALUMNO/datos_alumno.txt"
+Nombre: Ian Mario Villan
 Division: 113
 EOF
 
 # Obtener información del sistema
-echo "Obteniendo información del sistema..."
 IP=$(hostname -I | awk '{print $1}')
 DISTRO=$(lsb_release -d | cut -f2)
 CORES=$(nproc)
 
-# Generar datos del equipo
 echo "Generando datos del equipo..."
-cat <<EOF > /tmp/2do_parcial/equipo/datos_equipo.txt
+cat <<EOF > "$DIRECTORIO_EQUIPO/datos_equipo.txt"
 Ip: $IP
 Distribucion: $DISTRO
 Cantidad de cores: $CORES
 EOF
 
-# Crear el grupo 2PSupervisores si no existe
-echo "Comprobando si el grupo 2PSupervisores existe..."
-if ! getent group 2PSupervisores > /dev/null; then
-    echo "Creando el grupo 2PSupervisores..."
-    sudo groupadd 2PSupervisores
-else
-    echo "El grupo 2PSupervisores ya existe."
-fi
-
-# Configurar sudoers para el grupo 2PSupervisores
 echo "Configurando sudoers para el grupo 2PSupervisores..."
-sudo bash -c 'echo "%2PSupervisores ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers'
+if ! grep -q "^%2PSupervisores" /etc/sudoers; then
+    echo "%2PSupervisores ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    echo "Configuración de sudoers actualizada."
+else
+    echo "El grupo 2PSupervisores ya tiene configuración en sudoers."
+fi
 
 echo "Tareas completadas."
